374464c99489645658385b1aea3a75acc207b0f4
sarvalabs-sai <sai@sarva.ai>
2023-05-02 03:01:12 +0530

    chore: handled `--deployContract` case

--- backend/deployLogic.js
+++ backend/deployLogic.js
@@ -1,3 +1,34 @@
-// 1. import manifest
-// 2. Use MOI JS function to deploy contract/logic
-// 3. Add LogicID to .env
+const { LogicFactory } = require("moi.js/packages/moi-logic");
+const manifest = require("./api/manifests/moq.json");
+/**
+ *
+ * @param {JsonRpcProvider} provider RPC provioder
+ * @param {String} signer Signer address
+ * @returns logicID
+ */
+module.exports = async function(provider, signer) {
+    const deployerOptions = {
+        builderName: "MOQ!",
+        arguments: [
+            100
+        ]
+    }
+    const _lFactory = new LogicFactory(manifest, provider);
+    return new Promise((resolve, reject) => {
+        _lFactory.deploy(
+            deployerOptions, 
+            (err) => reject(err)
+        ).send({
+            sender: signer,
+            fuelPrice: "0x130D41",
+            fuelLimit: "0x130D41"
+        }).then(async ixResponse => {
+            try {
+                const logicObject = await ixResponse.result(ixResponse.hash);
+                resolve(logicObject.logic_id);
+            }catch(e) {
+                reject(e);
+            }
+        }).catch(err => reject(err))
+    })
+}
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
-// listen at 3004
+const fs = require('fs');
+const { argv } = require("yargs");
+const envfile = require('envfile');
+const { JsonRpcProvider } = require("moi.js/packages/moi-providers");
+
+const deployContract = require("./deployLogic");
+
+const encContent = fs.readFileSync(".env");
+let parsedEnvContent = envfile.parse(encContent.toString());
+
+let provider;
+if(parsedEnvContent.RPC_PROVIDER) {
+    provider = new JsonRpcProvider(parsedEnvContent.RPC_PROVIDER);
+}else {
+    throw new Error("no provider found");
+}
+
+if(argv.deployContract) {
+    let logicID = await deployContract(provider, parsedEnvContent.SIGNER_ADDRESS);
+    parsedEnvContent.MOQ_LOGIC_ID = logicID
+    return fs.writeFileSync('.env', envfile.stringify(parsedEnvContent)) 
+}
+
+console.log("Other case coming soon...!");
